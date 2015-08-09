a04701239fa0796169a7475d55b8c37c95036372
Alan Ayoub <alanayoub@gmail.com>
2015-08-09 19:12:48 +0100

    Adding old blog files recovered

---
+++ old_blog/hack/shBrushXml.js
@@ -0,0 +1,84 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 8:32:45 Dec 17, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:42:29 Aug 9, 2015.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/**
+ * SyntaxHighlighter
+ * /web/20131217083245/http://alexgorbatchev.com/
+ *
+ * SyntaxHighlighter is donationware. If you are using it, please donate.
+ * /web/20131217083245/http://alexgorbatchev.com/wiki/SyntaxHighlighter:Donate
+ *
+ * @version
+ * 2.0.320 (May 03 2009)
+ * 
+ * @copyright
+ * Copyright (C) 2004-2009 Alex Gorbatchev.
+ *
+ * @license
+ * This file is part of SyntaxHighlighter.
+ * 
+ * SyntaxHighlighter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SyntaxHighlighter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SyntaxHighlighter.  If not, see </web/20131217083245/http://www.gnu.org/copyleft/lesser.html>.
+ */
+SyntaxHighlighter.brushes.Xml = function()
+{
+	function process(match, regexInfo)
+	{
+		var constructor = SyntaxHighlighter.Match,
+			code = match[0],
+			tag = new XRegExp('(&lt;|<)[\\s\\/\\?]*(?<name>[:\\w-\\.]+)', 'xg').exec(code),
+			result = []
+			;
+		
+		if (match.attributes != null) 
+		{
+			var attributes,
+				regex = new XRegExp('(?<name> [\\w:\\-\\.]+)' +
+									'\\s*=\\s*' +
+									'(?<value> ".*?"|\'.*?\'|\\w+)',
+									'xg');
+
+			while ((attributes = regex.exec(code)) != null) 
+			{
+				result.push(new constructor(attributes.name, match.index + attributes.index, 'color1'));
+				result.push(new constructor(attributes.value, match.index + attributes.index + attributes[0].indexOf(attributes.value), 'string'));
+			}
+		}
+
+		if (tag != null)
+			result.push(
+				new constructor(tag.name, match.index + tag[0].indexOf(tag.name), 'keyword')
+			);
+
+		return result;
+	}
+	
+	this.regexList = [
+		{ regex: new XRegExp('(\\&lt;|<)\\!\\[[\\w\\s]*?\\[(.|\\s)*?\\]\\](\\&gt;|>)', 'gm'),			css: 'color2' },	// <![ ... [ ... ]]>
+		{ regex: new XRegExp('(\\&lt;|<)!--\\s*.*?\\s*--(\\&gt;|>)', 'gm'),								css: 'comments' },	// <!-- ... -->
+		{ regex: new XRegExp('(&lt;|<)[\\s\\/\\?]*(\\w+)(?<attributes>.*?)[\\s\\/\\?]*(&gt;|>)', 'sg'), func: process }
+	];
+};
+
+SyntaxHighlighter.brushes.Xml.prototype	= new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.Xml.aliases	= ['xml', 'xhtml', 'xslt', 'html', 'xhtml'];
---
+++ old_blog/png/shBrushJScript.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 10:25:57 Feb 3, 2011 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 13:13:01 Aug 9, 2015.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/**
+ * SyntaxHighlighter
+ * /web/20110203102557/http://alexgorbatchev.com/
+ *
+ * SyntaxHighlighter is donationware. If you are using it, please donate.
+ * /web/20110203102557/http://alexgorbatchev.com/wiki/SyntaxHighlighter:Donate
+ *
+ * @version
+ * 2.0.320 (May 03 2009)
+ * 
+ * @copyright
+ * Copyright (C) 2004-2009 Alex Gorbatchev.
+ *
+ * @license
+ * This file is part of SyntaxHighlighter.
+ * 
+ * SyntaxHighlighter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SyntaxHighlighter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SyntaxHighlighter.  If not, see </web/20110203102557/http://www.gnu.org/copyleft/lesser.html>.
+ */
+SyntaxHighlighter.brushes.JScript = function()
+{
+	var keywords =	'break case catch continue ' +
+					'default delete do else false  ' +
+					'for function if in instanceof ' +
+					'new null return super switch ' +
+					'this throw true try typeof var while with'
+					;
+
+	this.regexList = [
+		{ regex: SyntaxHighlighter.regexLib.singleLineCComments,	css: 'comments' },			// one line comments
+		{ regex: SyntaxHighlighter.regexLib.multiLineCComments,		css: 'comments' },			// multiline comments
+		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },			// double quoted strings
+		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },			// single quoted strings
+		{ regex: /\s*#.*/gm,										css: 'preprocessor' },		// preprocessor tags like #region and #endregion
+		{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' }			// keywords
+		];
+	
+	this.forHtmlScript(SyntaxHighlighter.regexLib.scriptScriptTags);
+};
+
+SyntaxHighlighter.brushes.JScript.prototype	= new SyntaxHighlighter.Highlighter();
+SyntaxHighlighter.brushes.JScript.aliases	= ['js', 'jscript', 'javascript'];
